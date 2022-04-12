b38736dcdf62c6c8c264020f76834e857665da76
Dalis8283 <84297882+Dalis8283@users.noreply.github.com>
2022-04-12 20:29:50 +0700

    Add index.html file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-let text="Bạn nghĩ bản thân mình có nghu không?";
-let result=text.fontsize(10);
-document.body.innerHTML=result;
+// let text="Bạn nghĩ bản thân mình có ngu không?";
+// let result=text.font("helvetica");
+// document.body.innerHTML=result;
+
 
 let clicked=0;
 
@@ -8,9 +9,13 @@ function btn1onclick(){
 	clicked+=1;
 	// alert("ok i got it!");
 	if (clicked==1){
-		document.getElementById("btn_1").innerHTML="Không, dĩ nhiên";
+		document.getElementById("main-text").textContent="Có, chắc chắn";
 	}
 	if (clicked>1){
-		alert("ok we got it!")
+		alert("Ok chúng tôi xác nhận điều đó!")
 	}
-	}
+}
+
+function btn2onclick(){
+	alert("Ừ tôi biết điều đó!");
+}
