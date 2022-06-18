3128bb591761532a6b2004efc499f80355f5350f
Dalis8283 <84297882+Dalis8283@users.noreply.github.com>
2022-06-18 12:59:09 +0700

    Add js/main.js file

--- js/main.js
+++ js/main.js
@@ -12,6 +12,19 @@ const email = "quangdv8283@gmail.com";
 const newTitle = "Stupidity Test";
 document.title = newTitle;
 
+const b = document.getElementById("btn_3");
+b.addEventListener("mouseover", moveHover);
+
+function moveHover(){
+	const i = Math.floor(Math.random()*300)+10;
+	const j = Math.floor(Math.random()*300)+10;
+
+	b.style.left = -i+"px";
+	b.style.right = i+"px";
+	b.style.top = j+"px";
+	b.style.bottom= -j+"px";
+}
+
 let clicked=0;
 
 function btn1onclick(){
@@ -35,4 +48,35 @@ function btn2onclick(){
 }
 function txtclick(){
 	alert("OK được rồi, bạn hãy xuống phía dưới trang web đi!!!");
+}
+
+function btn3onclick(){
+	alert("Chớt òi, bấm lại đi bạn");
+}
+
+function linkonclick(){
+	alert("Ok chúng tôi biết mà:))");
+}
+let click = 0;
+function link2onclick(){
+	// let a = document.createElement("a");
+	// a.setAttribute("href","https://www.meme-arsenal.com/memes/b31a9a96ad5ee99bf46e599167634eb0.jpg");
+	// a.setAttribute("target","_BLANK");
+	// a.innerHTML = "Xin lỗi đây mới là link!!!";
+	// document.getElementsByClassName("container_3")[0].appendChild(a);
+	// a.setAttribute("margin-left","400px");
+
+	click+=1;
+	const l = document.getElementById("trollLink");
+	if (click == 1){
+		l.innerHTML = "Xin lỗi link đây";
+		l.setAttribute("href","https://youtu.be/dQw4w9WgXcQ");
+		click == 1;
+	}
+	if (click > 1){
+		// l.innerHTML = "Xin l";
+		l.setAttribute("href","https://www.meme-arsenal.com/memes/b31a9a96ad5ee99bf46e599167634eb0.jpg");
+	}
+	
+	
 }
